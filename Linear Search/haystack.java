50df8babbf60439afc968c3aa919424ae8041ff8
V-Pradhan <pradhanvinayak5@gmail.com>
2026-02-02 18:32:48 +0530

    day31

---
+++ Linear Search/haystack.java
@@ -0,0 +1,24 @@
+public class haystack {
+    
+    public static int strStr(String haystack, String needle) {
+
+        int n = haystack.length();
+        int m = needle.length();
+
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack.substring(i, i + m).equals(needle)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strStr("sadbutsad", "sad"));
+        System.out.println(strStr("leetcode", "leeto"));
+    }
+
+
+    
+}
